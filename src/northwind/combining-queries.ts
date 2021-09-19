e6bd604eb770eb3f6b78c22142ed6bf37f4d97ed
ofadiman <ofadiman@gmail.com>
2021-09-19 16:59:52 +0200

    feat: finish section about combining queries

---
+++ src/northwind/combining-queries.ts
@@ -0,0 +1,7 @@
+import { query } from "../query"
+
+void query(`SELECT companyname FROM customers UNION SELECT companyname FROM suppliers;`)
+
+void query(`(SELECT city, country FROM customers INTERSECT SELECT city, country FROM suppliers) ORDER BY country;`)
+
+void query(`SELECT COUNT(*) FROM (SELECT city FROM customers INTERSECT SELECT city FROM suppliers) AS same_city;`)
