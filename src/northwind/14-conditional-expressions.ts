737f83f742f425cc318541ceb70e82fa15c29bf8
ofadiman <ofadiman@gmail.com>
2021-09-22 17:23:35 +0200

    feat: finish section about conditional expressions

---
+++ src/northwind/14-conditional-expressions.ts
@@ -0,0 +1,23 @@
+import { query } from "../query"
+
+// Description: If/else statement using SQL query.
+void query(`
+SELECT company_name,
+       country,
+       CASE
+         WHEN country IN ( 'Austria', 'Germany', 'Poland', 'France',
+                           'Sweden', 'Italy', 'Spain', 'UK',
+                           'Ireland', 'Belgium', 'Finland', 'Norway',
+                           'Portugal', 'Switzerland' ) THEN 'Europe'
+         WHEN country IN ( 'Canada', 'Mexico', 'USA' ) THEN 'North America'
+         WHEN country IN ( 'Argentina', 'Brazil', 'Venezuela' ) THEN 'South America'
+         ELSE country
+       END AS continent
+FROM   customers;
+`)
+
+// Description: The `COALESCE` function is used to return a default value if selected column value is null.
+void query(`SELECT company_name, COALESCE(homepage,'Call to find') from suppliers;`)
+
+// Description: The `NULLIF` function is used to return a null if 2 values are equal.
+void query(`SELECT company_name, phone, COALESCE(NULLIF(homepage,''),'Need to call') FROM suppliers;`)
